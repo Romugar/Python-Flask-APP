ebeecb795ae5847b1c3ffd10e2b6abd3bae05fe2
romugar <roberto2925@hotmail.com>
2018-06-05 14:45:39 +0200

    App.py hello world

--- src/app.py
+++ src/app.py
@@ -1,1 +1,15 @@
+from flask import Flask
+
+
 __author__ = "roberto munoz garcia"
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_method():
+    return "Hello, world!"
+
+
+if __name__ == "__main__":
+    app.run(debug=app.config['DEBUG'], port=4990)
