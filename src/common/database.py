947238df0cfe2007119876c45e808b9455f62b99
romugar <roberto2925@hotmail.com>
2018-06-05 14:27:34 +0200

    Add mongo database source

---
+++ src/common/database.py
@@ -0,0 +1,8 @@
+import pymongo
+
+__author__ = "roberto munoz garcia"
+
+
+class Database(object):
+    URI = "mongodb://<ekpro>:<ekpro123>@ds247330.mlab.com:47330/heroku_7p7jgn5l"
+    DATABASE = None
