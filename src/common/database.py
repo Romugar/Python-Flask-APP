bb66c31a1db233c5468d8e577f7036c25d0dbf88
romugar <roberto2925@hotmail.com>
2018-06-05 15:02:19 +0200

    Add db init

--- src/common/database.py
+++ src/common/database.py
@@ -6,3 +6,8 @@ __author__ = "roberto munoz garcia"
 class Database(object):
     URI = "mongodb://<ekpro>:<ekpro123>@ds247330.mlab.com:47330/heroku_7p7jgn5l"
     DATABASE = None
+
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.DATABASE = client.get_database()
