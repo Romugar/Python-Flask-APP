1803c98d8d14b4e36b387dc2b7b4ba58f406a618
romugar <roberto2925@hotmail.com>
2018-06-08 09:46:02 +0200

    busqueda clientes

--- src/app.py
+++ src/app.py
@@ -88,7 +88,7 @@ def query():
         query_clients = Cliente(cif, direccion, poblacion, cp, provincia, parroquia_razon, diocesis, arciprestazgo, web, responsable, cargo, dni, tfno1, tfno2, email)
         result = query_clients.filter_and_find()
 
-        return render_template("find_clients.html", mensaje="hola")
+        return render_template("find_clients.html", mensaje=result)
     elif request.method == "GET" and session["email"] is not None:
         return render_template("home.html")
     else:
