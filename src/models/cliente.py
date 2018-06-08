360add596030abf69062a8c38bfe5cb61f2794a3
romugar <roberto2925@hotmail.com>
2018-06-08 16:07:13 +0200

    busqueda clientes

--- src/models/cliente.py
+++ src/models/cliente.py
@@ -51,5 +51,5 @@ class Cliente(object):
 
     def find_clients(self):
         json = self.json()
-        filtrado = {key: value for key, value in json.items() if value != ''}
+        filtrado = {key: value for key, value in json.items() if value != '' and key != "_id" and key !="fecha_alta"}
         return [client for client in Database.find(collection="clientes", query=filtrado)]
