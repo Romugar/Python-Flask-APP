eb1d375dec739e7b0b1f7132422937fb3797749d
romugar <roberto2925@hotmail.com>
2018-06-11 14:37:27 +0200

    edit clients

--- src/app.py
+++ src/app.py
@@ -106,7 +106,7 @@ def edit_clients(result):
 @app.route("/guardar_edicion", methods=["GET", "POST"])
 def save_edition():
     if request.method == "POST":
-        dat = request.data
+        dat = request.form
         return render_template("edit_clients.html", mensaje=dat)
     elif request.method == "GET" and session["email"] is not None:
         return render_template("home.html")
